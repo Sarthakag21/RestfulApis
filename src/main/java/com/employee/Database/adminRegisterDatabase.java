d0debb6f85c58710d547c6bdc6069faaafcc1531
Sarthak Agarwal <sarthakag2002@gmail.com>
2023-09-14 15:47:02 +0530

    first commit

--- src/main/java/com/employee/Database/adminRegisterDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.employee.Database;
-
-import com.employee.Entity.adminRegisterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface adminRegisterDatabase extends JpaRepository<adminRegisterEntity, Integer> {
-}
